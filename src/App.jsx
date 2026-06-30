591c100c5d40a81a07f5cee539690aac015c044f
Namugwanya Rahmah <namugwanyarahmah42@gmail.com>
2026-06-30 09:09:00 +0300

    Added first react assignment

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+
+  return(
+
+    <div>
+
+      <h1>My Favorite Things.</h1>
+
+      <p>NAME: Namugwanya Rahmah</p>
+
+      <p>FAVORITE FOOD: Pilao and Fried Chicken</p>
+
+      <p>FAVORITE HOBBY: Crocheting and Knitting</p>
+
+      <p>FAVORITE MOVIE: Bet</p>
+
+      <p>DREAM CAREER: Computer Scientist</p>
+
+    </div>
+
+  );
+
+}
+
+export default App;
